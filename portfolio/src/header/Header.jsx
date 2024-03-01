05311cf44f6600347dd798c7ede2754233281e23
Ramil <fathulislamovrr@gmail.com>
2024-03-02 02:55:07 +0500

    Delete .git from portfolio

---
+++ portfolio/src/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export function Header() {
+  return (
+    <div>
+		<StyleHeader />
+		</div>
+  )
+}
+
+const StyleHeader = styled.div`
+
+`
