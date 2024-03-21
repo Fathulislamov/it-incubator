23f7cf77887725cc2b4a74ea9a6b080f9cd42452
Ramil <fathulislamovrr@gmail.com>
2024-03-21 21:00:38 +0500

    begin adaptive skills

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -15,8 +15,8 @@ function App() {
       <Main />
       <Quote />
       <Projects />
-      {/*<Skills />
-      <AboutMe /> 
+      <Skills />
+     {/* <AboutMe /> 
 			<Contacts />
 			<Footer />
 			*/}
