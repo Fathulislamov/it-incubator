cb52f4e39180cfccaa8b6b29f9809edbfaf4a668
Ramil <fathulislamovrr@gmail.com>
2024-04-21 18:21:51 +0500

    The first lesson

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
 import './App.css';
+import { TaskType, TodoList } from './TodoList';
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+  const todoListTitle_1 = "What to learn"
+  const todoListTitle_2 = "What to buy"
+
+  const tasks_1: Array<TaskType> = [
+    { id: 1, title: 'HTML & CSS', isDone: true },
+    { id: 2, title: 'JS & TS', isDone: true },
+    { id: 3, title: 'REACT', isDone: false },
+  ]
+  const tasks_2: Array<TaskType> = [
+    { id: 1, title: 'Ice cream', isDone: false},
+    { id: 2, title: 'Milk', isDone: true },
+    { id: 3, title: 'Chocolate', isDone: true },
+    { id: 4, title: 'Coca-Cola', isDone: true },
+  ]
+  return (
+    <div className="App">
+      <TodoList title={todoListTitle_1} tasks={tasks_1} />
+      <TodoList title={todoListTitle_2} tasks={tasks_2} />
+    </div>
+  );
 }
 
 export default App;
---
+++ todolist/src/TodoList.tsx
@@ -0,0 +1,50 @@
+type TodoListPropsType = {
+  title: string
+  tasks: Array<TaskType>
+}
+
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+export const TodoList = ({
+  title,
+  tasks
+}: TodoListPropsType) => {
+  // 1.
+  // const title = props.title
+  // const tasks = props.tasks
+
+  // 2. 
+  // const { title, tasks } = props
+
+  const taskList: Array<JSX.Element> = tasks.map(task => {
+    return (
+      <li>
+        <input type="checkbox" checked={task.isDone} />
+        <span>{task.title}</span>
+      </li>
+    )
+  })
+
+
+  return (
+    <div className="todolist">
+      <h3>{title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        {taskList}
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  )
+}
