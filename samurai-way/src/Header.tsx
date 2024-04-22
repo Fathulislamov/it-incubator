923d255d584afa029b05f7ee5caa98a3bc54360d
Ramil <fathulislamovrr@gmail.com>
2024-04-22 21:52:09 +0500

    First commit

---
+++ samurai-way/src/Header.tsx
@@ -0,0 +1,10 @@
+
+export const Header = (props: {}) => {
+  return (
+    <div>
+      <a href="#s">Home</a> - 
+      <a href="#s">News Feed</a> - 
+      <a href="#s">Messages</a> - 
+    </div>
+  )
+}
---
+++ samurai-way/src/Technologies.tsx
@@ -0,0 +1,13 @@
+
+export const Technologies = (props: {}) => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  )
+}
