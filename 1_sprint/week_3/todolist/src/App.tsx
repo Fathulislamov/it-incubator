25d7dbfca0dcca326b51c26817d5f258a8046899
Ramil <fathulislamovrr@gmail.com>
2024-05-05 21:21:46 +0500

    todolist lesson 3

---
+++ 1_sprint/week_3/todolist/src/App.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { v1 } from 'uuid';
+import './App.css';
+import { TaskType, TodoList } from './TodoList';
+
+
+export type FilterValuesType = 'all' | 'active' | 'completed'
+function App() {
+
+  //BLL
+  const todoListTitle = "What to learn"
+
+  // global state
+  const [tasks, setTasks] = useState<Array<TaskType>>([
+    { id: v1(), title: 'HTML & CSS', isDone: true },
+    { id: v1(), title: 'JS & TS', isDone: true },
+    { id: v1(), title: 'REACT', isDone: false },
+  ])
+
+  const removeTask = (taskId: string) => {
+
+    // const newState = []
+    // for (let i = 0; i < tasks.length; i++) {
+    //   if (tasks[i].id !== taskId) {
+    //     newState.push(tasks[i])
+    //   }
+    // }
+
+    const newState = tasks.filter(t => t.id !== taskId)
+    setTasks(newState)
+  }
+
+  const addTask = (newTaskTitle: string) => {
+    const newTask: TaskType = {
+      id: v1(),
+      title: newTaskTitle,
+      isDone: false
+    }
+    const newState = [newTask, ...tasks]
+    setTasks(newState)
+  }
+
+  return (
+    <div className="App">
+      <TodoList
+        title={todoListTitle}
+        tasks={tasks}
+        removeTask={removeTask}
+      />
+    </div>
+  );
+}
+
+export default App;
