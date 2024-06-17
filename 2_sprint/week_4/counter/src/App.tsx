909a0cbb15ce4f266690f15f9652427ead7bb2fe
Fathulislamov <Fathulislamovrr@gmail.com>
2024-06-17 19:58:10 +0500

    Counter complete

--- 2_sprint/week_4/counter/src/App.tsx
+++ 2_sprint/week_4/counter/src/App.tsx
@@ -9,22 +9,38 @@ export type SetValuesType = {
 }
 
 function App() {
+  const readLocalStorage = () => {
+    const InitialState: SetValuesType = {
+      maxValue: 5,
+      startValue: 0
+    }
+    const res = localStorage.getItem('state')
+    return res ? JSON.parse(res) : InitialState
+  }
 
-  const [value, setValue] = useState<SetValuesType>({ maxValue: 5, startValue: 0 })
+  const writeLocalStorage = (state: SetValuesType) => {
+    return localStorage.setItem('state', JSON.stringify(state))
+  }
+
+  const [value, setValue] = useState<SetValuesType>(readLocalStorage())
+
   const [error, setError] = useState<string>('')
   const [isNewData, setIsNewData] = useState<boolean>(false)
 
   const onChangeValue = (value: SetValuesType) => {
     setValue(value)
-    if(!isNewData) setIsNewData(true)
+    if (!isNewData) setIsNewData(true)
   }
+
 
   const setNewDataHandler = () => {
     setIsNewData(false)
+    writeLocalStorage(value)
   }
+
   return (
     <div className="App">
-      <Settings  onChangeValue={onChangeValue} currentValue={value} error={error} setError={setError} isNewData={isNewData} setNewDataHandler={setNewDataHandler} />
+      <Settings onChangeValue={onChangeValue} currentValue={value} error={error} setError={setError} isNewData={isNewData} setNewDataHandler={setNewDataHandler} />
       <Counter startValue={value.startValue} maxValue={value.maxValue} error={error} newData={isNewData} />
     </div >
   );
