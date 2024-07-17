8ef29fb757846d22195c3f3ad069c6926be5ec06
Fathulislamov <Fathulislamovrr@gmail.com>
2024-07-17 14:12:52 +0500

    4 sprint 1 week lesson microtaski

--- 4_sprint/1_week/microtaski/src/components/pages/Prices.tsx
+++ 4_sprint/1_week/microtaski/src/components/pages/Prices.tsx
@@ -4,75 +4,92 @@ import styles from './Prices.module.css';
 
 type Props = {};
 export const Prices = (props: Props) => {
-    const sneakers = [
-        {
-            manufacturer: "Adidas",
-            name: "Adidas Yeezy Boost 350",
-            price: 300,
-            onSale: false
-        },
-        {
-            manufacturer: "Adidas",
-            name: "Adidas Ultraboost 21",
-            price: 180,
-            onSale: true
-        },
-        {
-            manufacturer: "Puma",
-            name: "Puma Suede Classic",
-            price: 65,
-            onSale: true
-        },
-        {
-            manufacturer: "Puma",
-            name: "Puma RS-X",
-            price: 110,
-            onSale: false
-        },
-        {
-            manufacturer: "Abibas",
-            name: "Abibas Alphabounce",
-            price: 60,
-            onSale: true
-        }
-    ];
-
-    const [filteredSneakers, setFilteredSneakers] = useState(sneakers);
-
-    function handleOnSale() {
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sneakers = [
+    {
+      manufacturer: "Adidas",
+      name: "Adidas Yeezy Boost 350",
+      price: 300,
+      onSale: false
+    },
+    {
+      manufacturer: "Adidas",
+      name: "Adidas Ultraboost 21",
+      price: 180,
+      onSale: true
+    },
+    {
+      manufacturer: "Puma",
+      name: "Puma Suede Classic",
+      price: 65,
+      onSale: true
+    },
+    {
+      manufacturer: "Puma",
+      name: "Puma RS-X",
+      price: 110,
+      onSale: false
+    },
+    {
+      manufacturer: "Abibas",
+      name: "Abibas Alphabounce",
+      price: 60,
+      onSale: true
     }
+  ];
 
-    function handleReset() {
+  const [filteredSneakers, setFilteredSneakers] = useState(sneakers);
 
+
+  function handleOnSale() {
+    const saledSneakers = sneakers.filter(sneaker => sneaker.onSale)
+
+    setSearchParams({ onSale: 'true' })
+    // setFilteredSneakers(saledSneakers)
+  }
+
+  function handleReset() {
+
+    setSearchParams({})
+    setFilteredSneakers(sneakers)
+  }
+
+
+  useEffect(() => {
+    if (searchParams.get('onSale') === 'true') {
+      const saledSneakers = sneakers.filter(sneaker => sneaker.onSale)
+      setFilteredSneakers(saledSneakers)
+    } else {
+      setFilteredSneakers(sneakers)
     }
+  }, [searchParams.get('onSale')])
 
 
-    return (
-        <div>
-            <button onClick={handleOnSale} className={styles.buttonStyle}>On sale</button>
-            <button onClick={handleReset} className={styles.buttonStyle}>Reset filter</button>
+  return (
+    <div>
+      <button onClick={handleOnSale} className={styles.buttonStyle}>On sale</button>
+      <button onClick={handleReset} className={styles.buttonStyle}>Reset filter</button>
 
-            <table className={styles.tableStyle}>
-                <thead>
-                <tr>
-                    <th className={styles.thStyle}>Manufacturer</th>
-                    <th className={styles.thStyle}>Name</th>
-                    <th className={styles.thStyle}>Price</th>
-                    <th className={styles.thStyle}>On Sale</th>
-                </tr>
-                </thead>
-                <tbody>
-                {filteredSneakers.map((sneaker, index) => (
-                    <tr key={index}>
-                        <td className={styles.tdStyle}>{sneaker.manufacturer}</td>
-                        <td className={styles.tdStyle}>{sneaker.name}</td>
-                        <td className={styles.tdStyle}>${sneaker.price}</td>
-                        <td className={styles.tdStyle}>{sneaker.onSale ? 'Yes' : 'No'}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-    );
+      <table className={styles.tableStyle}>
+        <thead>
+          <tr>
+            <th className={styles.thStyle}>Manufacturer</th>
+            <th className={styles.thStyle}>Name</th>
+            <th className={styles.thStyle}>Price</th>
+            <th className={styles.thStyle}>On Sale</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredSneakers.map((sneaker, index) => (
+            <tr key={index}>
+              <td className={styles.tdStyle}>{sneaker.manufacturer}</td>
+              <td className={styles.tdStyle}>{sneaker.name}</td>
+              <td className={styles.tdStyle}>${sneaker.price}</td>
+              <td className={styles.tdStyle}>{sneaker.onSale ? 'Yes' : 'No'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
