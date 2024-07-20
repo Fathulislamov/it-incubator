999b4fb18d355554ad25c29be48cf135a8078f6c
Fathulislamov <Fathulislamovrr@gmail.com>
2024-07-20 22:06:12 +0500

    bugfix

--- 4_sprint/1_week/homework/src/s2-homeworks/hw09/Clock.tsx
+++ 4_sprint/1_week/homework/src/s2-homeworks/hw09/Clock.tsx
@@ -7,12 +7,12 @@ function Clock() {
   const [timerId, setTimerId] = useState<number | undefined>(undefined)
   // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
   const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
-  const [show, setShow] = useState<boolean>(true)
+  const [show, setShow] = useState<boolean>(false)
 
   const start = () => {
 
     const idTimer = setInterval(() => {
-      setDate(date => new Date(Date.now()))
+      setDate(new Date(Date.now()))
     }, 1000);
 
     setTimerId(idTimer as unknown as number)
@@ -35,10 +35,15 @@ function Clock() {
     setShow(false)
   }
 
+  const normalize = (val: number) => {
+    return val < 10 ? `0${val}` : val
+  }
+
+
   // const stringTime = 'date->time' || <br /> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-  const stringTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}` || <br /> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+  const stringTime = `${date.getHours()}:${normalize(date.getMinutes())}:${normalize(date.getSeconds())}` || <br /> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
   // const stringDate = 'date->date' || <br /> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-  const stringDate = `${date.getDay()} . ${date.getMonth()}.${date.getFullYear()}` || <br /> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+  const stringDate = `${normalize(date.getDate())}.${normalize(date.getMonth()+1)}.${date.getFullYear()}` || <br /> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
   // const stringDay = 'date->day' || <br /> // пишут студенты
