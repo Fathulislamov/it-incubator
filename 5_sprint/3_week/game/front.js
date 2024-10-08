1f4afc9a02d2f21238b05a5a80b1c64f2447d09d
Ramil <fathulislamovrr@gmail.com>
2024-10-09 04:18:22 +0500

    5 sprint 3 week lesson game complete 0.44

--- 5_sprint/3_week/game/front.js
+++ 5_sprint/3_week/game/front.js
@@ -1,2 +1,17 @@
+import { Game } from './game.js'
 
-console.log('hello,game')
+const game = new Game()
+game.settings.gridSize = {
+	rows: 4,
+	columns: 4
+}
+
+const tableElement = document.getElementById('grid')
+for (let y = 1; y <= game.settings.gridSize.rows; y++) {
+	const trElement = document.createElement('tr')
+	tableElement.append(trElement)
+	for (let x = 1; x <= game.settings.gridSize.columns; x++) {
+		const tdElement = document.createElement('td')
+		trElement.append(tdElement)
+	}
+}
