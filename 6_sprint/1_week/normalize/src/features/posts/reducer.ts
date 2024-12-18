cac51c6c5f7ad4d8667461b917df077173b8c0cd
Ramil <fathulislamovrr@gmail.com>
2024-12-18 15:54:52 +0500

    6 sprint 1 week lesson normalise continue

--- 6_sprint/1_week/normalize/src/features/posts/reducer.ts
+++ 6_sprint/1_week/normalize/src/features/posts/reducer.ts
@@ -4,15 +4,25 @@ import { api, PostType } from "../../api/api"
 const initialState = {
 	items: [] as PostType[]
 }
-export const postsReducer = (state = initialState, action: any) => {
-	return state
+export const postsReducer = (state = initialState, action: ReturnType<typeof fetchPostsSuccess>) => {
+	switch (action.type) {
+		case 'fetchPostsSuccess':
+			return {
+				...state,
+				items: action.payload.posts,
+			}
+		default: return state
+
+
+	}
 }
 
 export const fetchPostsSuccess = (posts: PostType[]) => ({
 	type: 'fetchPostsSuccess',
 	payload: { posts }
-})
+} as const)
+
 export const fetchPosts = () => async (dispatch: Dispatch) => {
 	const posts = await api.getPosts()
-	dispatch(fetchPostsSuccess(posts)
+	dispatch(fetchPostsSuccess(posts))
 }
