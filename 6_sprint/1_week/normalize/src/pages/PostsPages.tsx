2df95ac56a9c8369fef28cbd416abf64d49f4bf0
Ramil <fathulislamovrr@gmail.com>
2024-12-19 05:28:06 +0500

    6 sprint 1 week lesson normalize continue

--- 6_sprint/1_week/normalize/src/pages/PostsPages.tsx
+++ 6_sprint/1_week/normalize/src/pages/PostsPages.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
 import { useSelector } from "react-redux"
 import { AppStateType } from "../features/app/store"
 import { Post } from "../features/posts/components/Post"
+import { fetchPosts } from "../features/posts/reducer"
 
 export const PostsPages = () => {
+
 	const items = useSelector((state: AppStateType) => state.posts.items)
+
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(fetchPosts())
+	}, [])
+
 	return (
 		<div>
 			{items.map(i => <Post key={i.id} post={i} />)}
