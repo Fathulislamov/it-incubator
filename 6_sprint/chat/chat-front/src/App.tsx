2cd6e9ba87ffc223682ca0ed5024694ab984cf86
Ramil <fathulislamovrr@gmail.com>
2025-01-03 18:20:40 +0500

    6 sorint lesson socket io complete 0.58

--- 6_sprint/chat/chat-front/src/App.tsx
+++ 6_sprint/chat/chat-front/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	useEffect(() => {
+		const socket = io()
+
+	}, [])
+	const [messages, setMessages] = useState([
+		{
+			message: 'Hello Victor',
+			id: 'kjflkjdslfja',
+			user: {
+				id: 'jflkjdsa',
+				name: 'Dymuch'
+			}
+		},
+		{
+			message: 'Hello Dimuch',
+			id: 'kjflkkjfdsljja',
+			user: {
+				id: 'jflfjdsljf',
+				name: 'Victor'
+			}
+		},
+	]);
+	return (
+		<div className="App">
+			<div style={{
+				border: '1px solid black', padding: '10px', height: '300px', width: '300px', margin: '0 auto', overflowY: 'scroll'
+			}}>
+				{
+					messages.map(m => {
+						return <div>
+							<b>{m.user.name}:</b> <b>{m.message}</b>
+							<hr />
+						</div>
+					})
+				}
+			</div>
+			<textarea name="" id=""></textarea>
+			<button>Send</button>
+		</div >
+	);
 }
 
 export default App;
